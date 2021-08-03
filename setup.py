efa2547a6ae486fecb36c07d7511af1929f185ca
Multivalence <mulwanishiva@gmail.com>
2021-08-02 21:00:35 -0400

    increment version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="nikel-py",
-    version="0.1.5",
+    version="0.1.6",
     description="Python API Wrapper for the Nikel API",
     long_description=long_description,
     long_description_content_type="text/markdown",
